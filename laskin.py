277ff7638fa020bc9b2bce2acdca4d92ed4f1037
tynij <janne.tyni@uef.fi>
2026-03-27 10:49:56 +0200

    lisätty laskin.py

---
+++ laskin.py
@@ -0,0 +1,13 @@
+class Ostoskori:
+    def __init__(self, alkusumma):
+        self.summa = alkusumma
+        self.koodi = ""
+
+    def lisaa_koodi(self, koodi):
+        self.koodi = koodi
+
+    def laske_hinta(self):
+        #ALE10 antaa 10% alennuksen, jos summa on yli 50 euroa
+        if self.summa >= 50 and self.koodi == "ALE10":
+            return self.summa * 0.9
+        return self.summa
